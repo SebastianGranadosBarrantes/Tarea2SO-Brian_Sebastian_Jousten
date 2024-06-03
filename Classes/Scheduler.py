a7454f843722af8d06f7e5b3a11ab021342fdbc3
Brian Zeledón <111306278+Z3E-Brian@users.noreply.github.com>
2024-06-02 23:10:49 -0600

    Update Scheduler.py

--- Classes/Scheduler.py
+++ Classes/Scheduler.py
@@ -1,23 +1,18 @@
-
-
 class Scheduler:
     def __init__(self):
-        self.methods = ["SJF","HRRN","FIFO","FT"]
+        self.methods = ["SJF","FIFO","HRRN"]
 
-    def sort_process_list_ft(self,process_list):
+    #Shortest Job First
+    def sort_process_list_sjf(self,process_list):
         return process_list.sort(key=lambda x: x.finishTime)
 
-    # metodo para odenar por tiempo restante mas corto primero (este deberia estar siempre activo)
-    def sort_process_list_sjf(self,process_list):
-        return process_list.sort(key=lambda x: x.processName)
+    #Primero en entrar primero en salir
+    def sort_process_list_FIFO(self,process_list):
+        return process_list.sort(key=lambda x: x.idProcess)
 
-    # metodo para ordenar por taza repuesta mas alta
+    #Taza repuesta mas alta
     def sort_process_list_hrrn(self,process_list):
         return process_list.sort(key=lambda x: ((x.awaitingTime + x.executionTime) / x.executionTime),reverse=True)
 
     def getMethods(self):
         return self.methods
-
-
-def sort_process_list_sjf():
-    return None
